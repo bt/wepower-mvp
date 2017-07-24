7ef9834e1fbe2e18a18d7ed2a770aaf4f8826a5d
Vytautas Alkimavicius <vytautas.alkimavicius@metasite.net>
2017-07-24 15:29:14 +0300

    add contracts deployment to demo config and scp of app

--- smart-contracts/truffle.js
+++ smart-contracts/truffle.js
@@ -4,6 +4,11 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+    demo : {
+      host: "192.168.50.157",
+      port: 8545,
+      network_id: "8888"
     }
   }
 };
