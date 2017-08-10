c12b66162741a01eaaa21111484a6e9ae4bf7533
tomasg <tomas.giedraitis@metasite.net>
2017-08-10 09:47:52 +0300

    Add units of measurement (kWh / kW) to prediction graphs

--- web-frontend/src/app/registration/prediction-review/prediction-review.component.ts
+++ web-frontend/src/app/registration/prediction-review/prediction-review.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter, ViewChild } from '@angular/core'
+import {ActivatedRoute} from "@angular/router";
 
 import * as moment from 'moment'
 import { PredictionData } from "./prediction-data"
@@ -12,7 +13,6 @@ import { DataFiller } from "../../shared/data-filler.service"
   styleUrls: ['./prediction-review.component.scss'],
 })
 export class PredictionReviewComponent implements OnInit {
-
 
   backDisabled : boolean
   frontDisabled : boolean
@@ -185,7 +185,6 @@ export class PredictionReviewComponent implements OnInit {
           fontFamily: "'Roboto', 'Sans-serif'",
           fontSize: 20,
           fontColor: '#000',
-
           padding: 30
         },
         gridLines: {
@@ -234,7 +233,9 @@ export class PredictionReviewComponent implements OnInit {
   public lineChartType:string = 'line';
 
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {
+    this.route = route;
+  }
 
   ngOnInit() {
     let periodStart = moment().startOf('week').add(1, 'days')
@@ -328,6 +329,17 @@ export class PredictionReviewComponent implements OnInit {
 
     this.lineChartOptions.scales.yAxes[0].ticks.suggestedMin = lowestPoint
     this.lineChartOptions.scales.yAxes[0].ticks.suggestedMax = highestPoint
+
+    // get current route
+    const url = this.route.snapshot.url.join('/');
+
+    // change ticks callback function to display units of measure
+    this.lineChartOptions.scales.yAxes[0].ticks.callback = function (value) {
+      var units: string = ' kW'
+      url.includes('consumer') ? units += 'h' : null;
+
+      return value + units;
+    };
 
     this.lineChartLabels = dayLabels;
     setTimeout(() => {
